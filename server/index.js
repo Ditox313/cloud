36d7a2ef4ae8234257f540e7914a5cc8bac0c38b
Xsodex <flex513513@gmail.com>
2023-08-08 11:41:39 +0300

    Сделал базовую регу

--- server/index.js
+++ server/index.js
@@ -8,8 +8,23 @@ const cors = require('cors');
 
 
 const app = express();
-
 const PORT = config.get('serverPort');
+
+
+// Регистрируем модуль bodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Регистрируем Morgan 
+app.use(morgan('dev'));
+
+
+// Регистрируем Cors
+app.use(cors());
+
+
+// Регистрируем роут auth
+app.use('/api/auth', authRoutes);
 
 const start = async ()=> {
     try {
@@ -32,23 +47,4 @@ const start = async ()=> {
 
 start()
 
-
-
-
-// Регистрируем модуль bodyParser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// Регистрируем Morgan 
-app.use(morgan('dev'));
-
-
-// Регистрируем Cors
-app.use(cors());
-
-
-
-
-// Регистрируем роут auth
-app.use('/api/auth', authRoutes);
 
